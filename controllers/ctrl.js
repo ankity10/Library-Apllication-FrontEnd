d8aac21c594ecf6e0744f7c4ed2634eef914d8f2
Ankit Yadav <ankitwrk@gmail.com>
2016-06-27 13:36:39 +0530

    Refractoring code

--- controllers/ctrl.js
+++ controllers/ctrl.js
@@ -2,13 +2,12 @@
 
 // var root_app = angular.module('root_app',['ui.router']);
 
-root_app.controller('homeCtrl',['$scope',function($scope){
-
+root_app.controller('homeCtrl', ['$scope', function ($scope) {
 
 
 }]);
 
-root_app.controller('loginCtrl',['$scope','$http','$location','$rootScope', function ($scope, $http, $location, $rootScope) {
+root_app.controller('loginCtrl', ['$scope', '$http', '$location', '$rootScope', function ($scope, $http, $location, $rootScope) {
 
     $scope.login = function () {
 
@@ -17,56 +16,46 @@ root_app.controller('loginCtrl',['$scope','$http','$location','$rootScope', func
         postObject.password = $("#login-password").val();
 
         $http({
-            url:'http://localhost:3000/api/login/',
-            dataType:'json',
-            method:'POST',
-            data:postObject,
+            url: 'http://localhost:3000/api/login/',
+            dataType: 'json',
+            method: 'POST',
+            data: postObject,
             headers: {
                 "Content-Type": "application/json"
             }
         })
-            .success(function(response){
-
-                console.log("In success");
-
+            .success(function (response) {
+                // console.log("In success");
                 $scope.response = response;
+                // console.log($scope.response);
 
-                console.log($scope.response);
-
-                //var token_slice = response.token.split('.');
-                //var new_token = token_slice[0] + token_slice[1];
-
-                if(response.success){
-
+                if (response.success) {
                     $rootScope.user = {};
                     $rootScope.user.authenticated = true;
-
                     $location.path('/genre');
-
                     $http({
-                        url:'http://localhost:3000/api/dash',
-                        method:"GET",
-                        headers:{
-                            "Authorization":response.token
+                        url: 'http://localhost:3000/api/dash',
+                        method: "GET",
+                        headers: {
+                            "Authorization": response.token
                         }
                     })
-
                         .success(function (res) {
                             console.log(res);
                         })
-
                 }
-
-                else{
+                else {
                     $location.path('/verify');
                 }
 
-            }).error(function(error){
-            console.log("In error");
-            $scope.error = error;
-            console.log($scope.error);
+            })
 
-        });
+            .error(function (error) {
+                console.log("In error");
+                $scope.error = error;
+                console.log($scope.error);
+
+            });
 
 
     };
@@ -74,25 +63,25 @@ root_app.controller('loginCtrl',['$scope','$http','$location','$rootScope', func
 
 }]);
 
-root_app.controller('forget_passwordCtrl',['$scope','$http',function ($scope,$http) {
+root_app.controller('forget_passwordCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.forget_password = function () {
         var postObject = new Object();
         postObject.username = $("#email").val();
         $http({
-            url:'http://localhost:3000/api/resetpassword/',
-            dataType:'json',
-            method:'POST',
-            data:postObject,
+            url: 'http://localhost:3000/api/resetpassword/',
+            dataType: 'json',
+            method: 'POST',
+            data: postObject,
             headers: {
                 "Content-Type": "application/json"
             }
-        }).success(function(response){
+        }).success(function (response) {
             // console.log("In reset");
             $scope.response = response;
             console.log($scope.response);
 
-        }).error(function(error){
+        }).error(function (error) {
             console.log("In error");
             $scope.error = error;
             console.log($scope.error);
@@ -101,7 +90,7 @@ root_app.controller('forget_passwordCtrl',['$scope','$http',function ($scope,$ht
     };
 }]);
 
-root_app.controller('signupCtrl',['$scope', '$http',function ($scope,$http) {
+root_app.controller('signupCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.signup = function () {
         var postObject = new Object();
@@ -114,13 +103,13 @@ root_app.controller('signupCtrl',['$scope', '$http',function ($scope,$http) {
         console.log(postObject.password);
         $http({
             // url:'http://localhost:3000/api/register/',
-            dataType:'json',
-            method:'POST',
-            data:postObject,
+            dataType: 'json',
+            method: 'POST',
+            data: postObject,
             headers: {
                 "Content-Type": "application/json"
             }
-        }).success(function(response){
+        }).success(function (response) {
             // console.log("In register");
             $scope.response = response;
             console.log($scope.response);
@@ -135,7 +124,7 @@ root_app.controller('signupCtrl',['$scope', '$http',function ($scope,$http) {
             //         console.log(res);
             //     })
 
-        }).error(function(error){
+        }).error(function (error) {
             console.log("In error");
             $scope.error = error;
             console.log($scope.error);
@@ -151,7 +140,7 @@ root_app.controller('signupCtrl',['$scope', '$http',function ($scope,$http) {
         $http({
             url: 'http://localhost:3000/api/usercheck/',
             dataType: 'json',
-            method:'POST',
+            method: 'POST',
             data: getObject,
             headers: {
                 "Content-Type": "application/json"
@@ -173,38 +162,38 @@ root_app.controller('signupCtrl',['$scope', '$http',function ($scope,$http) {
         getObject.password = $("#login_password").val();
         getObject.passwordcheck = $("#login_password_match").val();
 
-            if(getObject.password==getObject.passwordcheck) {
-                console.log("Matched");
-            }
+        if (getObject.password == getObject.passwordcheck) {
+            console.log("Matched");
+        }
     }
 
-    
-}]);
-
-root_app.controller('genreCtrl',['$scope',function ($scope) {
-    
-}]);
-
-root_app.controller('aboutCtrl',['$scope', function ($scope) {
 
 }]);
 
-root_app.controller('upload_test',['$scope','$http', function($scope, $http){
+root_app.controller('genreCtrl', ['$scope', function ($scope) {
 
-    $scope.uploadFile = function(){
+}]);
+
+root_app.controller('aboutCtrl', ['$scope', function ($scope) {
+
+}]);
+
+root_app.controller('upload_test', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.uploadFile = function () {
         var file = $scope.myFile;
         var uploadUrl = "http://localhost:3000/api/upload";
         var fd = new FormData();
         fd.append('file', file);
 
-        $http.post(uploadUrl,fd, {
-                transformRequest: angular.identity,
-                headers: {'Content-Type': undefined}
-            })
-            .success(function(){
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+            .success(function () {
                 console.log("success!!");
             })
-            .error(function(){
+            .error(function () {
                 console.log("error!!");
             });
     };
--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,19 @@
 'use strict';
 
 
-var root_app = angular.module('root_app',['ui.router','angularFileUpload']);
-
+var root_app = angular.module('root_app', ['ui.router', 'angularFileUpload']);
 
 
 // ========================== directives start ===============================
 root_app.directive('fileModel', ['$parse', function ($parse) {
     return {
         restrict: 'A',
-        link: function(scope, element, attrs) {
+        link: function (scope, element, attrs) {
             var model = $parse(attrs.fileModel);
             var modelSetter = model.assign;
 
-            element.bind('change', function(){
-                scope.$apply(function(){
+            element.bind('change', function () {
+                scope.$apply(function () {
                     modelSetter(scope, element[0].files[0]);
                 });
             });
@@ -24,74 +23,71 @@ root_app.directive('fileModel', ['$parse', function ($parse) {
 // ========================== directives end ===============================
 
 
-
-
 //=================== Setting up route ======================================
-root_app.config(['$stateProvider','$urlRouterProvider','$httpProvider',
-  function($stateProvider,$urlRouterProvider,$httpProvider) {
+root_app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
+    function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
-      $httpProvider.defaults.useXDomain = true;
-      // $httpProvider.defaults.withCredentials = true;
-      delete $httpProvider.defaults.headers.common["X-Requested-With"];
-      $httpProvider.defaults.headers.common["Accept"] = "application/json";
-      $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+        $httpProvider.defaults.useXDomain = true;
+        // $httpProvider.defaults.withCredentials = true;
+        delete $httpProvider.defaults.headers.common["X-Requested-With"];
+        $httpProvider.defaults.headers.common["Accept"] = "application/json";
+        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 
-      $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/');
 
-    // states for my app
-    $stateProvider
-        .state('home',{
-            url:'/',
-            templateUrl:'views/home.html',
-            controller:'homeCtrl'
+        // states for my app
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'views/home.html',
+                controller: 'homeCtrl'
 
-        })
+            })
 
-        .state('login',{
-            url:'/login',
-            templateUrl:'views/login.html',
-            controller:'loginCtrl'
+            .state('login', {
+                url: '/login',
+                templateUrl: 'views/login.html',
+                controller: 'loginCtrl'
 
-        })
+            })
 
-        .state('genre',{
-            url:'/genre',
-            templateUrl:'views/genre.html',
-            controller:'genreCtrl'
-        })
+            .state('genre', {
+                url: '/genre',
+                templateUrl: 'views/genre.html',
+                controller: 'genreCtrl'
+            })
 
-        .state('verify',{
-            url:'/verify',
-            templateUrl:'views/verify.html'
-        })
+            .state('verify', {
+                url: '/verify',
+                templateUrl: 'views/verify.html'
+            })
 
-        .state('signup',{
-            url:'/signup',
-            templateUrl:'views/signup.html',
-            controller:'signupCtrl'
-        })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'views/signup.html',
+                controller: 'signupCtrl'
+            })
 
-        .state('about',{
-            url:'/about',
-            templateUrl:'views/about.html',
-            controller:'aboutCtrl'
-        })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'views/about.html',
+                controller: 'aboutCtrl'
+            })
 
-       
 
-        .state('forget_password',{
-          url:'/forget_password',
-          templateUrl:'views/forget_password.html',
-          controller:'forget_passwordCtrl'
+            .state('forget_password', {
+                url: '/forget_password',
+                templateUrl: 'views/forget_password.html',
+                controller: 'forget_passwordCtrl'
 
-      })
-        .state('upload_test',{
-            url:'/upload-test',
-            templateUrl:'views/upload_test.html',
-            controller:'upload_test'
-        });
+            })
+            .state('upload_test', {
+                url: '/upload-test',
+                templateUrl: 'views/upload_test.html',
+                controller: 'upload_test'
+            });
 
-  }
+    }
 ]);
 
 // =================== route ends ===========================================
